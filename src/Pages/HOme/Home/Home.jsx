c911dedd823d62da65eca43e97cb460633584451
shahisrail <shahisrail34@gmail.com>
2023-12-22 19:59:51 +0600

    add a faq and about sedctoin

--- src/Pages/HOme/Home/Home.jsx
+++ src/Pages/HOme/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Uses from '../../Uses/Uses';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Uses></Uses>
         </div>
     );
 };
--- src/Pages/Uses/Uses.jsx
+++ src/Pages/Uses/Uses.jsx
@@ -3,7 +3,41 @@ import React from 'react';
 const Uses = () => {
     return (
         <div>
+           
             <h2 className='text-3xl text-center'>Who uses and website can be of benefit</h2>
+            <div>
+      <h2>Benefits of a To-Do List Web Page</h2>
+      <ul className='text-3xl text-center'>
+        <li>
+          <h3>Individuals and Professionals:</h3>
+          <ul>
+            <li>Students: Manage study tasks, assignments, and deadlines.</li>
+            <li>Professionals: Organize work tasks, meetings, and projects.</li>
+            <li>Freelancers: Track project deadlines, client meetings, and deliverables.</li>
+            <li>Entrepreneurs: Plan tasks, prioritize objectives, and manage business-related activities.</li>
+            <li>Parents: Keep track of household chores, children's activities, and appointments.</li>
+          </ul>
+        </li>
+        <li>
+          <h3>Team Collaboration and Project Management:</h3>
+          <ul>
+            <li>Small Businesses: Coordinate tasks among team members, assign responsibilities, and track progress.</li>
+            <li>Startups: Manage development tasks, marketing initiatives, and launch schedules.</li>
+            <li>Remote Teams: Facilitate collaboration by sharing to-do lists, assigning tasks, and monitoring team progress.</li>
+            <li>Agile Development Teams: Utilize task boards to manage sprints and development tasks.</li>
+          </ul>
+        </li>
+        <li>
+          <h3>Specialized Use Cases:</h3>
+          <ul>
+            <li>Event Planners: Organize event schedules, tasks, and vendor coordination.</li>
+            <li>Fitness Enthusiasts: Create workout plans, track fitness goals, and set exercise reminders.</li>
+            <li>Healthcare Professionals: Manage patient appointments, treatment plans, and medical tasks.</li>
+          </ul>
+        </li>
+
+      </ul>
+    </div>
         </div>
     );
 };
